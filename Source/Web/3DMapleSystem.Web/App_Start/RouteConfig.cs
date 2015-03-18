75f0a1299b77c19c06ca8ba280285602dadc5494
Dr4g0 <d_d0003@yahoo.com>
2015-03-18 15:14:07 +0200

    Added administration area, fixed "area bug"

--- Source/Web/3DMapleSystem.Web/App_Start/RouteConfig.cs
+++ Source/Web/3DMapleSystem.Web/App_Start/RouteConfig.cs
@@ -14,9 +14,10 @@ namespace _3DMapleSystem.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "3DMapleSystem.Web.Controllers" }
             );
         }
     }
