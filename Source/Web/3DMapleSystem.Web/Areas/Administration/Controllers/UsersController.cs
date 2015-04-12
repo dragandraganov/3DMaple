a538ef8910396c1fdf8c6808d00a7666d6d3c56c
Dr4g0 <d_d0003@yahoo.com>
2015-04-12 21:32:51 +0300

    Added UsersController with Index and Edit Views, edit only available for download models

---
+++ Source/Web/3DMapleSystem.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using _3DMapleSystem.Data;
+using AutoMapper.QueryableExtensions;
+using _3DMapleSystem.Web.Areas.Administration.ViewModels;
+using System.Web;
+using AutoMapper;
+
+namespace _3DMapleSystem.Web.Areas.Administration.Controllers
+{
+    public class UsersController : AdminController
+    {
+        public UsersController(_3DMapleSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Administration/Users
+        public ActionResult Index()
+        {
+            var allUsers = this.Data
+               .Users
+               .AllWithDeleted()
+               .Project()
+               .To<UserViewModel>();
+
+            return View(allUsers);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var existingUser = this.Data
+                .Users
+                .AllWithDeleted()
+                .Where(u => u.Id == id)
+                .Project()
+                .To<UserViewModel>()
+                .FirstOrDefault();
+
+            if (existingUser == null)
+            {
+                throw new HttpException(404, "User not found");
+            }
+
+            return View(existingUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserViewModel user)
+        {
+            if (user != null && ModelState.IsValid)
+            {
+                var existingUser = this.Data
+                    .Users
+                    .AllWithDeleted()
+                    .FirstOrDefault(u => u.Id == user.Id);
+
+                existingUser.AvailableFreeModels = user.AvailableFreeModels;
+                existingUser.AvailableProModels = user.AvailableProModels;
+
+                this.Data.Users.Update(existingUser);
+                this.Data.SaveChanges();
+
+                TempData["Success"] = "The info for user '" + existingUser.UserName + "' was updated";
+
+                return RedirectToAction("Index", "Users");
+
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ Source/Web/3DMapleSystem.Web/Areas/Administration/ViewModels/UserViewModel.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using _3DMapleSystem.Data.Models;
+using _3DMapleSystem.Web.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3DMapleSystem.Web.Areas.Administration.ViewModels
+{
+    public class UserViewModel : IMapFrom<User>, IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public int? PhotoId { get; set; }
+
+        public AppFile Photo { get; set; }
+
+        public ICollection<PolyModel> OwnPolyModels { get; set; }
+
+        public ICollection<DownloadedPolyModelsUsers> DownloadedPolyModels { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+
+        public int AvailableFreeModels { get; set; }
+
+        public int AvailableProModels { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<User, UserViewModel>().ReverseMap();
+        }
+    }
+}
